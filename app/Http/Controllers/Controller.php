bcabbcc1174c782f123ee9dbcd937efc358f7880
Salvatore Seidita <salvatore.seidita@yahoo.com>
2018-02-17 22:09:23 +0000

    added controller method and view

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,59 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Show the the staff shifts asd requested.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show()
+    {
+      $main_query = DB::table('rota_slot_staff')
+        ->whereNotNull('staffid')
+        ->where('staffid', '<>', 0)
+        ->where('slottype', 'shift')
+        ->orderBy('staffid')
+        ->orderBy('daynumber');
+
+      $users = [];
+      $totals = [];
+
+      foreach ( $main_query->get() as $key => $record ) {
+        if( is_numeric($record->staffid) ) {
+
+          $users[$record->staffid] = $users[$record->staffid] ?? [];
+          if( is_numeric($record->daynumber) && $record->starttime && $record->endtime ) {
+            $users[$record->staffid][$record->daynumber] = $record->starttime.' - '.$record->endtime;
+            $totals[$record->daynumber] = $totals[$record->daynumber] ?? 0;
+            $totals[$record->daynumber] +=
+              round( abs( strtotime( $record->endtime ) - strtotime( $record->starttime ) ) / 3600,2);
+          }
+        }
+      }
+
+      ksort($totals);
+      $firstEmployee = min(array_keys($users));
+      $lastEmployee = max(array_keys($users));
+
+      return view('show',
+        [
+          'users' => $users,
+          'totals' => $totals,
+          'firstEmployee' => $firstEmployee,
+          'lastEmployee' => $lastEmployee,
+        ]
+      );
+
+    }
+
+
+
 }
---
+++ resources/views/show.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Staff Rota Shift</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <div class="card border-info m-3">
+      <div class="card-header bg-info text-white">
+        Staff Rota Shift
+      </div>
+
+      <div class="card-body">
+        <table class="table table-striped border text-center">
+          @foreach ( $users as $staffId => $shifts )
+            <!-- Print Header Row -->
+            @if ( $firstEmployee == $staffId )
+              <thead>
+                <tr>
+                  <th class="bg-secondary text-white"></th>
+                  @foreach ( $totals as $dayNumber => $totalPerDay )
+                    <th class="bg-secondary text-white"> {{ $dayNumber }} </th>
+                  @endforeach
+                </tr>
+              </thead>
+              <tbody>
+            @endif
+
+                <tr>
+                  <td class="bg-secondary text-white"> {{ $staffId }} </td>
+                  @foreach ( $totals as $dayNumber => $totalPerDay )
+                    @if ( array_key_exists( $dayNumber, $shifts ) )
+                      <td> {{ $shifts[$dayNumber] }} </td>
+                    @else
+                      <td></td>
+                    @endif
+                  @endforeach
+                </tr>
+
+                <!-- Print Totals Row -->
+                @if ( $lastEmployee == $staffId )
+                <tr>
+                  <td class="bg-secondary text-white">Totals</td>
+                  @foreach ( $totals as $dayNumber => $totalPerDay )
+                    <td class="bg-secondary text-white"> {{ $totalPerDay }} </td>
+                  @endforeach
+                </tr>
+              </tbody>
+            @endif
+          @endforeach
+        </table>
+      </div>
+
+      <div class="card-footer bg-info">
+      </div>
+    </div>
+  </body>
+</html>
